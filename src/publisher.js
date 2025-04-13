c1745a4ccda966a5ee0b3042e397e8b5b69ae294
Prachi-Jamdade <prachi.jamdade2390@gmail.com>
2025-04-13 15:32:54 +0530

    chore: log api key for testing

--- src/publisher.js
+++ src/publisher.js
@@ -8,7 +8,7 @@ const markdownBody = content.replace(/^# .*\n/, '');
 
 // ─── Dev.to ─────────────────────────────────────────────────────
 async function publishToDevto() {
-  console.log(process.env.DEVTO_API_KEY)
+  console.log("Hi", process.env.DEVTO_API_KEY)
   await axios.post('https://dev.to/api/articles', {
     article: {
       title,
